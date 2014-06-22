91b6aabfc68330e522f52beae5d78ed8790d37d1
SamCook42 <gitsam@samlr.com>
2014-06-22 15:54:43 +0100

    map & clone utils as well as some tidying

--- js/utils.js
+++ js/utils.js
@@ -5,53 +5,127 @@
 utils = (function() {
     'use strict';
 
-    function _arrayForEach (obj, callback) {
+    function _isEmpty(obj) {
+        var key;
+        if (obj === null || obj === undefined || typeof obj !== 'object') {
+            return true;
+        } else if (obj.length > 0) {
+            return false;
+        } else if (obj.length === 0) {
+            return true;
+        }
+
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function _isArray(obj) {
+        if ( typeof obj !== 'object' ) {
+            return false;
+        } else if (obj.hasOwnProperty('length')) {
+            return true;
+        } 
+        return false;
+    }
+
+    function _arrayForEach (arr, callback) {
+        var i, len = arr.length;
+        for (i = 0; i < len; i++) {
+            callback(arr[i], i, arr);
+        }
+        return arr;
+    }
+
+    function _objForEach (obj, callback) {
+        var key;
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                callback(obj[key], key, obj);
+            }
+        }
+        return obj;
+    }
+
+    function _forEach (obj, callback) {
+        if (_isArray(obj)) {
+            _arrayForEach(obj, callback);
+        } else {
+            _objForEach(obj, callback);
+        }
+        return obj;
+    }
+
+    function _map (obj, callback) {
+        var res = (_isArray(obj)) ? [] : {};
+        _forEach(obj, function (val, key, collection) {
+            res[key] = callback(val);
+        });
+        return res;
+    }
+
+    function _clone (obj, deepClone) {
+        // Recursively clone objects, copy values
+        var deepCloneFunc = function (val) {
+                if (typeof val === 'object') {
+                    return _clone(val);
+                } else {
+                    return val;
+                }
+            },
+            // copy values and references to objects
+            shallowCloneFunc = function (val) {
+                return val;
+            };
         
+        if (deepClone) {
+            return _map(obj, deepCloneFunc);
+        } else {
+            return _map(obj, shallowCloneFunc);
+        }
     }
 
     return {
-        forEach: function(obj, callback) {
-            var prop;
-            for (prop in obj) {
-                if (obj.hasOwnProperty(prop)) {
-                    
-                }
-            }
-        },
         /**
-         * Tests whether the object is empty (i.e. has no properties of its own)
+         * Make a copy of the object. If deepClone is specified this is done 
+         * recursively (but only with ownProperties).
+         * @param  {object} obj         The object to copy
+         * @param  {boolean} deepClone  Should the clone be deep
+         * @return {object}             The cloned object
+         */
+        clone: _clone,
+        /**
+         * Applies the callback function to every value in the object and returns
+         * a new object of the results
+         * 
+         * @param  {object}   obj
+         * @param  {Function} callback
+         * @return {object}
+         */
+        map: _map,
+        /**
+         * Apply the callback to every value:key/index pair in the object/array. 
+         * Callback is passed value:key/index:collection. Returns the collection.
+         * @param  {object}   obj
+         * @param  {function} callback to be applied to each pair
+         * @return {object}
+         */
+        forEach: _forEach,
+        /**
+         * Tests whether the object is empty (i.e. has no properties of its own).
+         * Returns true if something that's not an object is supplied.
          * @param  {object}  obj
          * @return {Boolean}
          */
-        isEmpty: function(obj) {
-            var prop;
-            if (obj === null || obj === undefined) {
-                return true;
-            } else if (obj.length > 0) {
-                return false;
-            } else if (obj.length === 0) {
-                return true;
-            }
-
-            for (prop in obj) {
-                if (obj.hasOwnProperty(prop)) {
-                    return false;
-                }
-            }
-            return true;
-        },
+        isEmpty: _isEmpty,
         /**
          * Is the object an array?
          * @param  {object}  obj
          * @return {Boolean}
          */
-        isArray: function(obj) {
-            if ( typeof obj !== 'object' ) {
-                return false;
-            } else if (obj.hasOwnProperty('length')) {
-                return true;
-            } 
-            return false;
-        }
+        isArray: _isArray
     };
 }());
--- test/test-utils.js
+++ test/test-utils.js
@@ -49,4 +49,124 @@
             utils.isEmpty({}).should.eql(true);
         });
     });
+
+    describe('forEach', function() {
+        it('should callback with value/key/collection correctly for an object', function() {
+            var test = {a: 1, b:2},
+                expect = [
+                    [1, 'a', {a: 1, b:2}],
+                    [2, 'b', {a: 1, b:2}]
+                ],
+                res = [];
+            utils.forEach(test, function () {
+                res.push(arguments);
+            });
+
+            res.should.have.a.lengthOf(2);
+            res[0].should.eql(expect[0]);
+            res[1].should.eql(expect[1]);
+        });
+
+        it('should callback with value/index/collection pairs correctly for an array', function() {
+            var test = [1, 2, 3],
+                expect = [
+                    [1, 0, [1, 2, 3]],
+                    [2, 1, [1, 2, 3]],
+                    [3, 2, [1, 2, 3]]
+                ],
+                res = [];
+            utils.forEach(test, function () {
+                res.push(arguments);
+            });
+
+            res.should.have.a.lengthOf(3);
+            res[0].should.eql(expect[0]);
+            res[1].should.eql(expect[1]);
+            res[2].should.eql(expect[2]);
+        });
+
+        it('should return what it\'s passed (for chaining)', function() {
+            utils.forEach([1,2,3], function(){}).should.eql([1,2,3]);
+            utils.forEach({foo:'bar'}, function(){}).should.eql({foo:'bar'});
+        });
+
+        it('should ignore inherited values', function () {
+            var a = {a:1, b:2},
+                b = Object.create(a),
+                res = [];
+
+            b.c = 3;
+
+            utils.forEach(b, function () {
+                res.push(arguments);
+            });
+            res.should.have.a.lengthOf(1);
+            res[0].should.eql([ 3, 'c', {c:3} ]); 
+
+        });
+    });
+
+    describe('map', function() {
+        it('should return a new object', function() {
+            utils.map({a:1, b:2}, function (val){
+                return val + 1;
+            }).should.eql({a:2, b:3});
+        });
+
+        it('should work with arrays', function() {
+            utils.map([1, 2], function (val){
+                return val + 1;
+            }).should.eql([2, 3]);
+        });
+    });
+
+    describe('clone', function() {
+        it('should clone the object', function() {
+            var test = {a: 1, b:2, c:3},
+                res = utils.clone(test);
+            res.should.eql(test);
+            res.c = 4;
+            res.should.not.eql(test);
+        });
+
+        it('deepclone should clone the object recursively', function() {
+            var test = {a: 1, b:2, c:{d:3, e:5}},
+                res = utils.clone(test, true);
+            res.should.eql(test);
+            res.c.d = 4;
+            res.should.not.eql(test);
+        });
+
+        it('should normally shallow clone the object', function() {
+            var test = {a: 1, b:2, c:{d:3, e:5}},
+                res = utils.clone(test);
+            res.should.eql(test);
+            res.c.d = 4;
+            res.should.eql(test);
+            res.b = 4;
+            res.should.not.eql(test);
+        });
+
+        it('should clone arrays shallowly', function() {
+            var test = [1,2, [3,2]],
+                res = utils.clone(test);
+
+            res.should.eql(test);
+            res[2][1]++;
+            res.should.eql(test);
+            res[1]++;
+            res.should.not.eql(test);
+        });
+
+        it('should clone arrays deeply', function() {
+            var test = [1,2, [3,2]],
+                res = utils.clone(test, true);
+
+            res.should.eql(test);
+            res[2][1]++;
+            res.should.not.eql(test);
+            res[1]++;
+            res.should.not.eql(test);
+        });
+    });
 }());
