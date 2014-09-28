c327d898a4996db9e069d272ea9424e4969d2149
SamCook42 <gitsam@samlr.com>
2014-09-28 22:10:18 +0100

    added graphics utils package

---
+++ js/graphics-utils.js
@@ -0,0 +1,54 @@
+graphicsUtils = (function() {
+    var _getBox = function(x, y, width, height, context, colour) {
+        var x2 = x + width,
+            y2 = y + height;
+        return {
+            x: function() { return x; },
+            y: function() { return y; },
+            x2: function() { return x2; },
+            y2: function() { return y2; },
+            width: function() { return width; },
+            height: function() { return height; },
+            setX: function(_x) {
+                x = _x;
+                x2 = _x + width;
+            },
+            setY: function(_y) {
+                y = _y;
+                y2 = _y + height;
+            },
+            draw: function() {
+                context.fillStyle = colour;
+                context.fillRect(
+                    this.x,
+                    this.y,
+                    this.width,
+                    this.height);
+                context.restore();
+            },
+            isCompletelyIn: function(other) {
+                // This is wholly within other
+                return (other.x() < this.x()) && (this.x2() < other.x2()) &&
+                       (other.y() < this.y()) && (this.y2() < other.y2());
+            },
+            isPartiallyIn: function(other) {
+                // There is some over lap between this & other
+                var xInBounds = other.x() < this.x() && this.x() < other.x2(),
+                    yInBounds = other.y() < this.y() && this.y() < other.y2(),
+                    x2InBounds = other.x() < this.x2() && this.x2() < other.x2(),
+                    y2InBounds = other.y() < this.y2() && this.y2() < other.y2(),
+                    skipInverse = arguments.length > 1;
+
+                return xInBounds && yInBounds   ||
+                       xInBounds && y2InBounds  ||
+                       x2InBounds && yInBounds  ||
+                       x2InBounds && y2InBounds ||
+                       !skipInverse && other.isPartiallyIn(this, true); // Check special case
+            }
+        };
+    };
+
+    return {
+        getBox: _getBox
+    };
+}());
---
+++ test/test-graphics-utils.js
@@ -0,0 +1,149 @@
+(function() {
+    'use strict';
+    var should = require('should');
+    require('../js/graphics-utils.js');
+
+    describe('getBox', function() {
+        var a = graphicsUtils.getBox(0, 0, 10, 10);
+
+        it('Should return something with position, width and height', function() {
+            a.should.have.properties('x', 'y', 'x2', 'y2', 'width', 'height');
+            a.x.should.be.a.Function;
+            a.x2.should.be.a.Function;
+            a.y.should.be.a.Function;
+            a.y2.should.be.a.Function;
+            a.width.should.be.a.Function;
+            a.height.should.be.a.Function;
+        });
+
+        it('Should have setters for x & y', function() {
+            a.should.have.properties('setX', 'setY');
+            a.setX.should.be.a.Function;
+            a.setY.should.be.a.Function;
+        });
+
+        it('Should have a "isCompletelyIn" function', function() {
+            a.should.have.property('isCompletelyIn');
+            a.isCompletelyIn.should.be.a.Function;
+        });
+
+        it('Should have a "isPartiallyIn" function', function() {
+            a.should.have.property('isPartiallyIn');
+            a.isPartiallyIn.should.be.a.Function;
+        });
+
+        it('Should have a "draw" function', function() {
+            a.should.have.property('draw');
+            a.draw.should.be.a.Function;
+        });
+    });
+
+    describe('Basic setters/getters', function() {
+        var x = 1,
+            y = 2,
+            width = 10,
+            height = 20,
+            a;
+        beforeEach(function() {
+            a = graphicsUtils.getBox(x, y, width, height);
+        });
+
+        describe('x, y, width, height', function() {
+            it('x, should return the x position', function() {
+                a.x().should.equal(x);
+            });
+            it('y, should return the y position', function() {
+                a.y().should.equal(y);
+            });
+            it('width, should return the width', function() {
+                a.width().should.equal(width);
+            });
+            it('height, should return the height', function() {
+                a.height().should.equal(height);
+            });
+        });
+
+        describe('x2, y2', function() {
+            it('x2() should return x + width',function() {
+                a.x2().should.equal(x+width);
+            });
+            it('y2() should return y + height',function() {
+                a.y2().should.equal(y+height);
+            });
+        });
+
+        describe('Setters', function() {
+            it('setX should update x and x2',function() {
+                var newX = 100;
+                a.x().should.equal(x);
+                a.setX(newX);
+                a.x().should.equal(newX);
+                a.x2().should.equal(newX+width);
+                a.width().should.equal(width);
+                // Nothing else should change
+                a.y().should.equal(y);
+                a.y2().should.equal(y+height);
+                a.height().should.equal(height);
+            });
+            it('setY should update y and y2',function() {
+                var newY = 100;
+                a.y().should.equal(y);
+                a.setY(newY);
+                a.y().should.equal(newY);
+                a.y2().should.equal(newY+height);
+                a.height().should.equal(height);
+                // Nothing else should change
+                a.x().should.equal(x);
+                a.x2().should.equal(x+width);
+                a.width().should.equal(width);
+            });
+        });
+
+    });
+
+    describe('isCompletelyIn', function() {
+        var a = graphicsUtils.getBox( 0,  0, 100, 100),
+            b = graphicsUtils.getBox(45, 45,  10,  10),
+            c = graphicsUtils.getBox( 0,  0,  10,  10),
+            d = graphicsUtils.getBox( 5,  5,  10,  10);
+
+        it('it should return true for a trivial case', function() {
+            b.isCompletelyIn(a).should.be.true;
+        });
+
+        it('it should return false for a trivial case', function() {
+            c.isCompletelyIn(b).should.be.false;
+        });
+
+        it('it should return false for being parent surrounding a child', function() {
+            a.isCompletelyIn(b).should.be.false;
+        });
+
+        it('it should return false for an overlap', function() {
+            d.isCompletelyIn(c).should.be.false;
+        });
+    });
+
+    describe('isPartiallyIn', function() {
+        var a = graphicsUtils.getBox( 0,  0, 100, 100),
+            b = graphicsUtils.getBox(45, 45,  10,  10),
+            c = graphicsUtils.getBox( 0,  0,  10,  10),
+            d = graphicsUtils.getBox( 5,  5,  10,  10);
+
+        it('it should return correctly for trivial cases', function() {
+            b.isPartiallyIn(a).should.be.true;
+        });
+
+        it('it should return false for a trivial case', function() {
+            c.isPartiallyIn(b).should.be.false;
+        });
+
+        it('it should return false for being parent surrounding a child', function() {
+            a.isPartiallyIn(b).should.be.true;
+        });
+
+        it('it should return true for an overlap', function() {
+            d.isPartiallyIn(c).should.be.true;
+        });
+    });
+}());
